4a96160553a661221c28e24857a01e1875551114
Ronnie Villarini <ronnie@ronini.dev>
2021-06-10 15:37:11 -0500

    custom header test

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const { getStoreData } = require('./api/storeData.js');
+const express = require("express");
+const cors = require("cors");
+const { getStoreData } = require("./api/storeData.js");
 
 const app = express();
 app.use(cors());
 
-app.get('/', async (req, res) => {
-  const storeData = await getStoreData();
+app.get("/", async (req, res) => {
+  const storeData = {
+    foo: "bar",
+  };
+  res.set("X-RateLimit-Limit", 15);
   res.json(storeData);
 });
 
